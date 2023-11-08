54472594be0d17339c778106115b46512da4cb51
Alison8547 <alisonailson100@hotmail.com>
2023-11-08 17:47:13 -0300

    feat: consumer emaildto

--- src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,15 +1,24 @@
 package com.ms.email.consumer;
 
+import com.ms.email.domain.Email;
 import com.ms.email.dto.EmailDto;
+import com.ms.email.service.EmailServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+    private final EmailServiceImpl emailService;
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailDto emailDto) {
-        // TODO
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email);
+        emailService.sendEmail(email);
     }
 }
